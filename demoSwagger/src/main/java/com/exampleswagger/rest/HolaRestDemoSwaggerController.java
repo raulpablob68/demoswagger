55255d03b6a870459dff8f2388aff8e516af1764
rPablo <raul.pablo.b@gmail.com>
2019-05-10 14:37:52 -0500

    Sexto commit.

--- demoSwagger/src/main/java/com/exampleswagger/rest/HolaRestDemoSwaggerController.java
+++ demoSwagger/src/main/java/com/exampleswagger/rest/HolaRestDemoSwaggerController.java
@@ -7,20 +7,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping
 public class HolaRestDemoSwaggerController {
-  @GetMapping("/api/1.0/hola")
+  @ApiOperation(value = "Return hola @ApiOperation.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 100, message = "Message 100."),
+      @ApiResponse(code = 200, message = "Successful saludos.")
+  })
+  @GetMapping("/api/1.0/saludos")
   public String hola() {
     return "Hola.";
   }
   
-  @PostMapping("/api/1.0/hola")
+  @PostMapping("/api/1.0/saludos")
   public String holaPost(@RequestBody final String hola) {
     return hola;
   }
   
-  @PutMapping("/api/1.0/hola")
+  @PutMapping("/api/1.0/saludos")
   public String holaPut(@RequestBody final String hola) {
     return hola;
   }
--- demoSwagger/src/main/java/com/exampleswagger/rest/UsuarioRestDemoSwaggerController.java
+++ demoSwagger/src/main/java/com/exampleswagger/rest/UsuarioRestDemoSwaggerController.java
@@ -18,12 +18,12 @@ public class UsuarioRestDemoSwaggerController {
    * 
    * @return Retorna una lista de usuarios.
    */
-  @GetMapping("/api/1.0/usuario")
+  @GetMapping("/api/1.0/usuarios")
   public List<Usuario> getUsuarios() {
     return Arrays.asList(new Usuario("Juan", 2000L), new Usuario("Pablo", 1000L));
   }
 
-  @GetMapping("/api/1.0/usuario/{nomUsu}")
+  @GetMapping("/api/1.0/usuarios/{nomUsu}")
   public Usuario getUsuario(@PathVariable("nomUsu") final String nomUsu) {
     return new Usuario(nomUsu, 1000L);
   }
