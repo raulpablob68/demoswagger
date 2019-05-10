a13882ba50848ac328f273cd2974f7b6469d0afc
rPablo <raul.pablo.b@gmail.com>
2019-05-10 14:12:19 -0500

    Quinto commit.

--- demoSwagger/src/main/java/com/exampleswagger/rest/HolaRestDemoSwaggerController.java
+++ demoSwagger/src/main/java/com/exampleswagger/rest/HolaRestDemoSwaggerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/1.0/hola")
+@RequestMapping
 public class HolaRestDemoSwaggerController {
   @GetMapping("/api/1.0/hola")
   public String hola() {
